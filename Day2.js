3645d2f72b1ac7ec6f7ea60de42721c813509adb
Office Laptop <sebghatullah1994@gmail.com>
2024-08-25 19:10:22 +0430

    practice

---
+++ Day2.js
@@ -0,0 +1,9 @@
+let Challenge = "30 Days Of JavaScript";
+console.log(Challenge);
+console.log(Challenge.length);
+console.log(Challenge.toUpperCase());
+console.log(Challenge.toLowerCase());
+console.log(Challenge.substring(0, 1));
+console.log(Challenge.substring(3));
+console.log(Challenge.includes("Script"));
+Challenge.split()
---
+++ app.js
@@ -0,0 +1,39 @@
+//comment can make code readable
+//wlcome to 30DaysOfJavaScript
+
+/*commments can make code readable,easy to reuse and informative
+
+*/
+
+let a = "ahmad";
+let b = true;
+let c = undefined;
+let d = null;
+
+console.log(typeof "a");
+
+let dummyVariable;
+let anotherONe;
+let thirdOne;
+let fourthVariablewithNoValue;
+
+let firstVar = 3;
+let secondVar = "a";
+let thirdVar = "4.5";
+let fourthVar = false;
+
+let firstName = "Sebghat";
+let LastName = "Amin";
+let Country = "Afghanistan";
+let Age = "30";
+
+let firstname = "sebghat",
+  lastname = "amin",
+  country = "afg",
+  age = 30;
+
+let myAge = "I am 25 years old.";
+let yourAge = "You are 30 years old.";
+
+console.log(myAge);
+console.log(yourAge);
